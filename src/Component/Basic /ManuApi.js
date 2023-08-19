128c6e70d27727ca4c69b0f1475052cc1b6038fe
Nilu patel <nilusingh945164@gmail.com>
2023-08-19 09:05:51 +0530

    project-3

---
+++ src/Component/Basic /ManuApi.js
@@ -0,0 +1,85 @@
+const menu = [
+    {
+        id: 1,
+        Image: "Image/maggi.jpg ",
+        name: "maggi",
+        categiry: "lunch",
+        prize: 25,
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Perferendis, repellendus? Assumenda quam quas nostrum fugiat."
+    },
+    {
+        id: 2,
+        Image: "Image/Momo.jpg ",
+        name: "momo",
+        categiry: "lunch",
+        prize: 25,
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Perferendis, repellendus? Assumenda quam quas nostrum fugiat."
+    },
+    {
+        id: 3,
+        Image: "Image/corn.jpeg ",
+        name: "corn",
+        categiry: "lunch",
+        prize: 20,
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Perferendis, repellendus? Assumenda quam quas nostrum fugiat."
+    },
+    {
+        id: 4,
+        Image: "Image/Alloparatha.jpeg ",
+        name: "Alloparatha",
+        categiry: "lunch",
+        prize: 22,
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Perferendis, repellendus? Assumenda quam quas nostrum fugiat."
+    },
+    {
+        id: 5,
+        Image: "Image/chola.jpeg ",
+        name: "chola",
+        categiry: "lunch",
+        prize: 25,
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Perferendis, repellendus? Assumenda quam quas nostrum fugiat."
+    },
+    {
+        id: 6,
+        Image: "Image/chomin.jpeg ",
+        name: "chomin",
+        categiry: "lunch",
+        prize: 25,
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Perferendis, repellendus? Assumenda quam quas nostrum fugiat."
+    },
+    {
+        id: 7,
+        Image: "Image/pavbhaji.jpeg ",
+        name: "pavbhaji",
+        categiry: "dinner",
+        prize: 250,
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Perferendis, repellendus? Assumenda quam quas nostrum fugiat."
+    },
+    {
+        id: 8,
+        Image: "Image/pizza.jpeg ",
+        name: "pizza",
+        categiry: "dinner",
+        prize: 100,
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Perferendis, repellendus? Assumenda quam quas nostrum fugiat."
+    },
+    {
+        id: 9,
+        Image: "Image/smosa.jpg ",
+        name: "smosa",
+        categiry: "lunch",
+        prize: 25,
+        Description: 
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Perferendis, repellendus? Assumenda quam quas nostrum fugiat."
+    },
+    {
+        id: 10,
+        Image: "Image/sweet.jpeg ",
+        name: "sweet",
+        categiry: "lunch",
+        prize: 25,
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Perferendis, repellendus? Assumenda quam quas nostrum fugiat."
+    },
+];
+
+export default menu;
--- src/Component/Basic /Reasurant.js
+++ src/Component/Basic /Reasurant.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import './style.css';
+import menu from './ManuApi.js';
 
 function Reasurant() {
-  return (
-    <div className="card-container">
-        <div className="card">
-            <div className="card-body">
-                <span className="card-number">1</span>
-                <span className="card-author">Breakfast</span>
+    const mystyle = { color: "red" }; //or write style={{color:"red"}} in span breakfast line
+    return (
+        <div className="card-container">
+            <div className="card">
+                <div className="card-body">
+                    <span className="card-number">1</span>
+                    <span className="card-author" style={mystyle}>Breakfast</span>
+                    <h1 className="card-title">Maggi</h1>
+                    <span className="card-description">
+                        I love maggi Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dolorum sequi incidunt explicabo sed voluptas consectetur
+                        voluptatibus labore enim accusamus fugit?
+                        
+                    </span>
+                    <div className="card-read">Read</div>
+                </div>
+                <img src="{Image}" alt="image" className='card-media' />
+                <div className="card-tag-subtle">Order Now</div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Reasurant
