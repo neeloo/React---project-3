52038ff24e18165a2a0c6dc5ca688278df6fd2cf
Nilu patel <nilusingh945164@gmail.com>
2023-08-27 10:34:40 +0530

    weather

--- src/Component/Weather App/Temp.js
+++ src/Component/Weather App/Temp.js
@@ -2,15 +2,45 @@ import React from 'react'
 import './style.css'
 
 const Temp = () => {
-  return (
-    <>
-    <div className="wrap">
-        <div className="search">
-    <h1>heloow</h1>
-        </div>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className="wrap">
+                <div className="search">
+                    <input
+                        type="search"
+                        placeholder="search..."
+                        autoFocus
+                        id="search"
+                        className="searchTerm"
+                    //value={searchValue}
+                    // onChange={(e) => setSearchValue(e.target.value)}
+                    />
+
+                    <button
+                        className="searchButton"
+                        type="button"
+                    //onClick={getWeatherInfo}
+                    >
+                        Search
+                    </button>
+                </div>
+            </div>
+
+            {/* our temp card */}
+            <article className='widget'>
+                <div className="weatherIcon">
+                    <i className={"wi  wi-day-sunny"}></i>
+                </div>
+                <div className="weatherInfo">
+                    <div className="Temprecture">
+                        <span>25.5deg</span>
+                    </div>
+                </div>
+            </article>
+
+            {/* <Weathercard {...tempInfo} /> */}
+        </>
+    )
 }
 
 export default Temp
