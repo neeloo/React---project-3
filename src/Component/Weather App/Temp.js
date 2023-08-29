a3246b4df6c1aac049cdeb1fcf6e4f995ad1c845
Nilu patel <nilusingh945164@gmail.com>
2023-08-29 17:29:25 +0530

    react

--- src/Component/Weather App/Temp.js
+++ src/Component/Weather App/Temp.js
@@ -1,116 +1,72 @@
-import React, { useEffect, useState } from 'react'
-import './style.css'
+import React, { useState, useEffect } from "react";
+import Weathercard from "./weathercard";
+import "./style.css";
 
 const Temp = () => {
+  const [searchValue, setSearchValue] = useState("pune");
+  const [tempInfo, setTempInfo] = useState({});
 
-    const [searchValue, setSearchValue] = useState("pune");
-    const getWeatherInfo = async () => {
-        try {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={WriteYourAPIKey}`;
-            const res = await fetch();
-            const data = await res.json();
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={WriteYourAPIKey}`;
 
-        } catch (error) {
-            console.log(error)
-        }
+      let res = await fetch(url);
+      let data = await res.json();
 
-    };
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
 
-    useEffect(() => {
-        getWeatherInfo();
-    }, [])
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
 
-    return (
-        <>
-            <div className="wrap">
-                <div className="search">
-                    <input
-                        type="search"
-                        placeholder="search..."
-                        autoFocus
-                        id="search"
-                        className="searchTerm"
-                        value={searchValue}
-                        // onChange={(e) => setSearchValue(e.target.value)}
-                        onChange={(e) => setSearchValue(e.target.value)}
-                    />
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-                    <button
-                        className="searchButton"
-                        type="button"
-                        onClick={getWeatherInfo}
-                    >
-                        Search
-                    </button>
-                </div>
-            </div>
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
 
-            {/* our temp card */}
-            <article className='widget'>
-                <div className="weatherIcon">
-                    <i className={"wi  wi-day-sunny"}></i>
-                </div>
-                <div className="weatherInfo">
-                    <div className="Temprecture">
-                        <span>25.5deg</span>
-                    </div>
-                    <div className="description">
-                        <div className="weatherCondition">sunny</div>
-                        <div className="place">pune,India</div>
-                    </div>
-                </div>
-                <div className="data">{new Date().toLocaleString()}</div>
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
 
-                {/* our 4 cloum section */}
-                <div className="extra-temp">
-                    <div className="temp-info-minmax">
-                        <div className="two-sided-section">
-                            <p><i className={" wi  wi-sunset"}></i>
-                            </p>
-                            <p className="extra-info-leftside">
-                                19:19 PM <br />
-                                Sunset
-                            </p>
-                        </div>
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}>
+            Search
+          </button>
+        </div>
+      </div>
 
-                        <div className="two-sided-section">
-                            <p><i className={" wi  wi-humidity"}></i>
-                            </p>
-                            <p className="extra-info-leftside">
-                                19:19 PM <br />
-                                humidity
-                            </p>
-                        </div>
-                    </div>
-                    <div className="weather-extra-info">
-                        <div className="two-sided-section">
-                            <p>
-                                <i className={"wi wi-rain"}></i>
-                            </p>
-                            <p className="extra-info-leftside">
-                                {/* {pressure}  */}
-                                <br />
-                                Pressure
-                            </p>
-                        </div>
+      {/* our temp card  */}
+      <Weathercard {...tempInfo} />
+    </>
+  );
+};
 
-                        <div className="two-sided-section">
-                            <p>
-                                <i className={"wi wi-strong-wind"}></i>
-                            </p>
-                            <p className="extra-info-leftside">
-                                {/* {speed}  */}
-                                <br />
-                                Speed
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </article>
-
-            {/* <Weathercard {...tempInfo} /> */}
-        </>
-    )
-}
-
-export default Temp
+export default Temp;
