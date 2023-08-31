af461062dec11db37e69b9a0e714d90b3883e441
Nilu patel <nilusingh945164@gmail.com>
2023-08-31 13:54:58 +0530

    react

--- src/Component/Weather App/Temp.js
+++ src/Component/Weather App/Temp.js
@@ -7,7 +7,7 @@ const Temp = () => {
   const [tempInfo, setTempInfo] = useState({});
 
   const getWeatherInfo = async () => {
-    
+
     try {
       let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={WriteYourAPIKey}`;
 
@@ -33,7 +33,7 @@ const Temp = () => {
 
       setTempInfo(myNewWeatherInfo);
     } catch (error) {
-      console.log(error);
+      //console.log(error);
     }
   };
 
