f5e5e54aa0f10be4f08657b558d85198696c7c44
Nilu patel <nilusingh945164@gmail.com>
2023-12-31 20:25:59 +0530

    p3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 // import ToDo from './Component/ToDoList/ToDo'
-//import Reasurant from './Component/Basic /Reasurant'
+// import Reasurant from './Component/Basic /Reasurant'
 // import UseState  from  './Component/Hook/UseState';
 // import  UseEffect from './Component/Hook/UseEffect';
 // import UseReduser from './Component/Hook/UseReduser';
-// import Temp from './Component/Weather App/Temp';
+ import Temp from './Component/Weather App/Temp';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       {/* <UseEffect/> */}
       {/* <UseReduser/> */}
       {/* <ToDo /> */}
-      {/* <Temp /> */}
+      <Temp />
       
     </>
   )
